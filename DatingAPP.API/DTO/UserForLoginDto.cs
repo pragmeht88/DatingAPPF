1149ce4d5b759f209e3a108d3a270352ed6116b5
pragmeht88 <pmehta.angulartutorials@gmail.com>
2020-01-26 20:01:31 -0800

    End of Section 3

---
+++ DatingAPP.API/DTO/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+namespace DatingAPP.API.DTO
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Pass { get; set; }
+    }
+}
--- DatingAPP.API/Startup.cs
+++ DatingAPP.API/Startup.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using DatingAPP.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingAPP.API
 {
@@ -32,6 +37,21 @@ namespace DatingAPP.API
 
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+           .AddJwtBearer(options =>
+           {
+               options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+               {
+                   ValidateIssuerSigningKey = true,
+                   IssuerSigningKey =
+                   new SymmetricSecurityKey(Encoding.ASCII.
+                   GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                   ValidateIssuer = false,
+                   ValidateAudience = false
+               };
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +67,8 @@ namespace DatingAPP.API
             app.UseRouting();
 
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
